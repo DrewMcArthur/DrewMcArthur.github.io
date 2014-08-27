11f8866d9a01b1f750e83b17af99c4f807caf8db
drewmcarthur <drewmcarthur1@gmail.com>
2014-08-27 17:29:05 -0400

    didn't do anything

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
 	$("#header").load("/pages/header.html");
 	$("#footer").load("/pages/footer.html");
+/*
+ * this doesn't work for some reason ???
+	$("#footer .drewmcarthur").on("click", function() {
+		console.log('clicked');
+		//window.location.href = '/'
+	});
+*/
 });
