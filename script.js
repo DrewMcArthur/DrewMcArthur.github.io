270fafedcc0003cddc2af5435984220a9c1b4ecb
drewmcarthur <drewmcarthur1@gmail.com>
2014-08-28 16:16:30 -0400

    works nicely

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$("#header").load("/pages/header.html");
-	$("#footer").load("/pages/footer.html");
+	$("#footer").load("/pages/footer.html").addClass('mini');
 /*
  * this doesn't work for some reason ???
 	$("#footer .drewmcarthur").on("click", function() {
@@ -8,4 +8,23 @@ $(document).ready(function(){
 		//window.location.href = '/'
 	});
 */
+	$('div#main').scroll(function() {
+		//header and footer grow/shrink functions.  Header starts big and footer starts small.
+		if($('div#main').scrollTop() > 0) {
+			//make header small
+			$('#header').addClass('mini');
+			console.log('header small');
+		} else {
+			$('#header').removeClass('mini');
+			console.log('header big');
+		}
+		if($('div#main').scrollTop() + $('div#main').height() == $(document).height()) {
+			//make footer full
+			$('#footer').removeClass('mini');
+			console.log('footer big');
+		} else {
+			$('#footer').addClass('mini');
+			console.log('footer small');
+		}
+	});
 });
