c8fe75df2e50800d9c3b622ed8200e49590ce235
Drew McArthur <drewmcarthur1@gmail.com>
2017-10-06 14:30:36 +0200

    added tiny background and removed react from index

--- script.js
+++
@@ -1,7 +0,0 @@
-import ProgressiveImage from "react-progressive-image-loading";
-
-<ProgressiveImage
-    preview="/assets/tiny-bg.png"
-    src="/assets/bg.png"
-    render={(src, style) => <body style={Object.assign(style, { backgroundImage: `url(${src})` })} />}
-/>
