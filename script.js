f01550c7a48451166ab1ee049b919b4ebe3bbcfc
Drew McArthur <drewmcarthur1@gmail.com>
2017-10-06 20:59:24 +0200

    fixed progressive loading

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-/* jQuery equivalent, but replacing with pure javascript
-$(document).load(function(){
-  $('div.tiny_bg').hide();
-});
-*/
-
-// once all images have loaded
-//document.addEventListener("load", function() { 
-$('.main').on('click', function(){
-  // hide the div that contains the blurred backgroud
-  document.getElementById("tiny_bg").style.animation = "loaded .5s cubic-bezier(0,0,1,1);";
-  //document.getElementById("tiny_bg").style.visibility = "hidden";
-
+// when the window is fully loaded, images included
+//$(window).load(function(){
+$(window).load(function(){
+  $('div#tiny_bg').addClass('faded');
 });
