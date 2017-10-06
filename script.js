5361a7b58d757d8479a43d3197de7fe4b6b991bf
Drew McArthur <drewmcarthur1@gmail.com>
2017-10-06 19:50:11 +0200

    javascript progressive loading, scale at 1.05

---
+++ script.js
@@ -0,0 +1,11 @@
+/* jQuery equivalent, but replacing with pure javascript
+$(document).load(function(){
+  $('div.tiny_bg').hide();
+});
+*/
+
+// once all images have loaded
+document.addEventListener("DOMContentLoaded", function(event) { 
+  // hide the div that contains the blurred backgroud
+  //document.getElementById("tiny_bg").style.visibility = "hidden";
+});
