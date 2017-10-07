09f289c4b7f861c7ba082fa0ab184d12b92cb2ce
Drew McArthur <drewmcarthur1@gmail.com>
2017-10-07 13:09:20 +0200

    added blurred image for smaller widths

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+/*
 // when the window is fully loaded, images included
 $(window).load(function(){
   // add the class containing the fadeout animation to the blurred background
   $('div#tiny_bg').addClass('faded');
 });
+*/
+
+/* javascript version of the code above */
+
+// when the window is fully loaded, images included
+window.onload = function(){
+  // add the class containing the fadeout animation to the blurred background
+  document.getElementById('tiny_bg').className += " faded";
+}
