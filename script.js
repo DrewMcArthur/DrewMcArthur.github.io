4a09cf9390217d5c5c0e036bae40cc7b9b48885c
Drew McArthur <drewmcarthur1@gmail.com>
2017-10-07 02:49:02 +0200

    update script with comments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // when the window is fully loaded, images included
-//$(window).load(function(){
 $(window).load(function(){
+  // add the class containing the fadeout animation to the blurred background
   $('div#tiny_bg').addClass('faded');
 });
