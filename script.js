ad951f238768f9c6acfcaf6808c1c411be9d1c2b
Drew McArthur <drewmcarthur1@gmail.com>
2017-10-21 15:03:44 +0200

    svg dl arrow

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ window.onload = function(){
   // add the class containing the fadeout animation to the blurred background
   document.getElementById('tiny_bg').className += "faded";
 }
+
+function showVcard() {
+  var els = document.getElementsByClassName('open')
+  var nEls = els.length;
+  if (nEls > 0) {
+    for (var i = 0; i < nEls; i++){
+      els[0].className += " close";
+      els[0].classList.remove("open");
+    }
+  } else { 
+    document.getElementsByClassName('vcard')[0].classList.remove("close");
+    document.getElementsByClassName('fa-address-card')[0].classList.remove("close");
+    document.getElementsByClassName('vcard')[0].className += " open";
+    document.getElementsByClassName('fa-address-card')[0].className += " open";
+  }
+}
