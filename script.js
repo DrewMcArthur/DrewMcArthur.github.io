114e9c88f68c2101d4223c2a27b5181b70c1af92
Drew McArthur <drewmcarthur1@gmail.com>
2017-12-09 17:33:12 +0100

    add functions to position and show hoverbar
    when hovering over ul.social-icons, show #hoverbar, and position it under the hovered icon and change its color appropriately.

--- script.js
+++ script.js
@@ -29,3 +29,36 @@ function showVcard() {
     document.getElementsByClassName('fa-address-card')[0].className += " open";
   }
 }
+
+var barLeftX = 0;
+
+function onHover(e) {
+  // when we hover over the social list, 
+  var hoverbar = document.getElementById('hoverbar');
+  var iconWidth = 32.5;
+  // show the hoverbarr
+  hoverbar.style.display = "block";
+  // get mouses position while hovering over the social list
+  x=e.clientX; 
+  // set mouses position, relative within the coordinates of the social list
+  barLeftX = x - 75;
+  // bring the hoverBar's left coordinate to the nearest multiple of 27
+  // (the width of the social icons) to place it under the left corner 
+  // of the hovered icon.  i.e. 29px is over the 1st indexed icon, so the left
+  // corner would be at x=27. 29 % 27 = 2, 29 - 2 = 27.
+  barLeftX -= (barLeftX % iconWidth);
+  // move the bar to that x coordinate
+  hoverbar.style.left = barLeftX+"px";
+  // change the color accordingly
+  var colors = ["#4676dc", "#eb8231", "#52a0fd", "#FFEC00", "#3D95CE", 
+                "#0073a4", "#000", "#9e3416"];
+  hoverbar.style.borderColor = colors[barLeftX / iconWidth];
+}
+
+function hideBar(){
+  // when we stop hovering over the social list, 
+  var hoverbar = document.getElementById('hoverbar');
+  // hide the hoverbarr
+  hoverbar.style.display = "none";
+  
+}
